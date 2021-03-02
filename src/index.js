91edc064160d61c9ca8627005cc89be86f56fec9
Sasha Petrov <brat77@tut.by>
2021-03-02 21:26:27 +0300

    init: decide task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  var minOfArray = 0;
+  if (!array) return 0;
+    for (var i = 0; i < array.length; i++ ) {
+      if ( minOfArray > array[i] ) minOfArray = array[i];
+    }
+  return minOfArray;
 }
 
 exports.max = function max (array) {
-  return 0;
+  var maxOfArray = 0;
+  if (!array) return 0;
+  for (var i = 0; i < array.length; i++ ) {
+    if ( maxOfArray < array[i] ) maxOfArray = array[i];
+  }
+  return maxOfArray;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+var avgOfArray = 0;
+if (!array) return 0;
+  for (var i = 0; i < array.length; i++ ) {
+    avgOfArray += array[i];
+  }
+  return avgOfArray/array.length||0;
 }
